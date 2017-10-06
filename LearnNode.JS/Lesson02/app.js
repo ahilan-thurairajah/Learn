72011a97fda277c720eff204b23049511d8cbdaf
Ahilan Thurairajah <athurairajah@cambridgefx.com>
2017-10-06 12:44:01 -0400

    Initial lessons from W3 Schools

---
+++ LearnNode.JS/Lesson02/app.js
@@ -0,0 +1,15 @@
+﻿var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var dt = require('./myfirstmodule.js');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<pre>Hello World! \n');
+    res.write(txt + "\n");
+    res.write('The date and time are currently: ' + dt.myDateTime() + '</pre>');
+    res.end();
+}).listen(8080);
+
